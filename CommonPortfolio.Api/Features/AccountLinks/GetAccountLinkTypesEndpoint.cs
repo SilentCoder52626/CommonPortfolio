330d9705afe1e5f9b3cf82044d55fbcf7a120f90
common khadka <commonkhadka@gmail.com>
2024-12-12 22:02:38 +0545

    Add AccountLink Enum and endpoint

---
+++ CommonPortfolio.Api/Features/AccountLinks/GetAccountLinkTypesEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using CommonPortfolio.Domain.Enums;
+
+namespace CommonPortfolio.Api.Features.AccountLinks
+{
+    public class GetAccountLinkTypesEndpoint : EndpointWithoutRequest<List<string>>
+    {
+        public override void Configure()
+        {
+            Get("/api/account-links-types"); 
+            AllowAnonymous();
+        }
+        public override async Task<List<string>> ExecuteAsync(CancellationToken ct)
+        {
+            var accountLinks = Enum.GetNames(typeof(AccountLinkEnum)).ToList();
+
+            return await Task.FromResult(accountLinks);
+        }
+    }
+}
---
+++ CommonPortfolio.Domain/Enums/AccountLinkEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace CommonPortfolio.Domain.Enums
+{
+    public enum AccountLinkEnum
+    {
+        Github = 1,
+        LinkedIn = 2,
+        Facebook = 3,
+        Twitter = 4,
+        Instagram = 5,
+        StackOverflow = 6,
+        YouTube = 7,
+        Reddit = 8,
+        Medium = 9,
+        Pinterest = 10,
+        Dev= 11,
+    }
+
+}
